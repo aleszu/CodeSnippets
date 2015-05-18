ce393607de4cfcaf29126677feabf1a8acba17a1
golfecholima <george.levines@gmail.com>
2015-05-18 00:53:15 -0400

    replaces scrollY with scrollTop()

--- FloatingAudioPlayer/js/audio-bug.js
+++ FloatingAudioPlayer/js/audio-bug.js
@@ -1,12 +1,16 @@
 
 // BRING IN YOUR AUDIO FILES HERE
 
+// plugin version would allow these to be input through CMS backend.
+
 var audioOne = 'https://d63kb4t2ifcex.cloudfront.net/201505horton/mp3/Horton_ImNotThisPicture.mp3';  // replace these with your audio files 
 var	audioTwo = 'https://d63kb4t2ifcex.cloudfront.net/201505horton/mp3/Horton_WeHadBackThen.mp3';  // replace these with your audio files
 var	audioThree = 'https://d63kb4t2ifcex.cloudfront.net/201505horton/mp3/Horton_IveNeverBeen.mp3';  // replace these with your audio files
 var	audioFour = 'https://d63kb4t2ifcex.cloudfront.net/201505horton/mp3/Dukakis_OneOfTheValuesofFurlough.mp3';  // replace these with your audio files
 
-// SET YOUR SCROLL BREAKPOINTS HERE
+// SET YOUR SCROLL BREAKPOINTS HERE 
+
+// for a plugin these might be set using a shortcode or empty span with a designated class
 
 var breakOne = 0;
 var	breakTwo = 1200;
@@ -15,10 +19,10 @@ var	breakFour = 2400;
 
 // SET YOUR AUDIO FILE TITLES
 
-var titleOne = 'LISTEN: This is the first audio file';
-var	titleTwo = 'LISTEN: This is the second audio file';
-var	titleThree = 'LISTEN: This is the third audio file';
-var	titleFour = 'LISTEN: This is the fourth audio file';
+var titleOne = 'LISTEN: The first audio file';
+var	titleTwo = 'LISTEN: The second audio file';
+var	titleThree = 'LISTEN: The third audio file';
+var	titleFour = 'LISTEN: The fourth audio file';
 
 // DO THE STUFF
 
@@ -44,59 +48,57 @@ var audioElement = document.createElement('audio');
     	}
     });
 
-	//hovering over the bug expands it to reveal the audio title. 
-
-		$('.audioBug').hover(function() {
-			$(this).addClass('expanded');
-			setTimeout(function() {
-				$('.audioBug > h6').fadeIn(500);	
-			}, 600);
-		}, function() {
-			$('.audioBug > h6').fadeOut(500);
-			setTimeout(function() {
-				$('.audioBug').removeClass('expanded');
-			}, 600);
-		});
-
-
-
 	// collapses the bug 5 seconds after loading
 	function removeBumper() {
 		setTimeout(function() {
 			$('.audioBug > h6').fadeOut(500, function() {
-				$('.audioBug').removeClass('expanded');
+				$('.audioBug').animate({width: '4rem'}, 'fast');
 			});
 		}, 5000);
 	}
 
+	//hovering over the bug expands it to reveal the audio title. 
+
+	$('.audioBug').hover(function() {
+		$(this).animate({
+			width: '28rem'}, 'fast');
+		$('.audioBug > h6').fadeIn('slow');
+	}, function() {
+		setTimeout(function() {
+			$('.audioBug > h6').fadeOut(500, function() {
+				$('.audioBug').animate({width: '4rem'}, 'fast');
+			});
+		}, 1000);
+	});
 
 	//page loads, audio bug expands to reveal title, then collapses again
+	setTimeout(function previewBug() {
+		$('.audioBug').animate({width: '28rem'}, 'fast');
+	}, 1000);
 	setTimeout(function() {
-		$('.audioBug').addClass('expanded');
-	}, 2000);
-	setTimeout(function() {
-		$('.audioBug > h6').fadeIn(500, function() {
+		$('.audioBug > h6').fadeIn(300, function() {
 			removeBumper();
 		});
 	}, 2700);
 
-	//scrolling past set points changes out the audio file/title and expands the bug anew
+	//scrolling through set ranges changes the audio file/title and expands the bug anew
 
-	window.addEventListener("scroll",function() { 
-	   if(window.scrollY > breakOne && window.scrollY < breakTwo) {
-	      audioElement.setAttribute('src', audioOne);
-	      $('h6.audio-title').text( titleOne );
-	   }
-	   else if(window.scrollY > breakTwo && window.scrollY < breakThree){
-	      audioElement.setAttribute('src', audioTwo);
-	      $('h6.audio-title').text( titleTwo );
-	   }
-	   else if(window.scrollY > breakThree && window.scrollY < breakFour){
-	      audioElement.setAttribute('src', audioThree);
-	      $('h6.audio-title').text( titleThree );
-	   }
-	   else if(window.scrollY > breakFour){
-	      audioElement.setAttribute('src', audioFour);
-	      $('h6.audio-title').text( titleFour );
-	   }
-	},false);
+    $(window).scroll(function() {
+    	if ($(this).scrollTop() > breakOne && $(this).scrollTop() < breakTwo) {
+    		audioElement.setAttribute('src', audioOne);
+    		$('.audioBug > h6').text( titleOne );
+    	}
+    	else if ($(this).scrollTop() > breakTwo && $(this).scrollTop() < breakThree) {
+    		audioElement.setAttribute('src', audioTwo);
+    		$('.audioBug > h6').text(titleTwo);
+    	}
+    	else if ($(this).scrollTop() > breakThree && $(this).scrollTop() < breakFour) {
+    		audioElement.setAttribute('src', audioThree);
+    		$('.audioBug > h6').text(titleThree);
+    	}
+    	else if ($(this).scrollTop() > breakFour) {
+    		audioElement.setAttribute('src', audioFour);
+    		$('.audioBug > h6').text(titleFour);
+    	}
+
+    });
